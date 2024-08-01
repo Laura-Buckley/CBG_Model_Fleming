a4c8adda5f94e2c12dbf7165821f0ce361b83960
Laura-Buckley <laura.buckley1@ucdconnect.ie>
2024-08-01 09:24:16 +0100

    removed recording of the extracellular mechanisms in soma ais etc in run_model.py

--- Cortex_BasalGanglia_DBS_model/run_model.py
+++ Cortex_BasalGanglia_DBS_model/run_model.py
@@ -211,11 +211,12 @@ if __name__ == "__main__":
     GPi_Pop.record("soma(0.5).v", sampling_interval=rec_sampling_interval)
     Thalamic_Pop.record("soma(0.5).v", sampling_interval=rec_sampling_interval)
 
-    if ctx_stimulation:
-        Cortical_Pop.record("soma(0.5).ref_e_extracellular", sampling_interval=rec_sampling_interval)
-        Cortical_Pop.record("ais(0.5).ref_e_extracellular", sampling_interval=rec_sampling_interval)
-        for n in num_axon_compartments:
-            Cortical_Pop.record("node[n](0.5).ref_e_extracellular", sampling_interval=rec_sampling_interval)
+    # need new way of measuring the e-extracellular applied
+    # if ctx_stimulation:
+    #     Cortical_Pop.record("soma(0.5).ref_e_extracellular", sampling_interval=rec_sampling_interval)
+    #     Cortical_Pop.record("ais(0.5).ref_e_extracellular", sampling_interval=rec_sampling_interval)
+    #     for n in num_axon_compartments:
+    #         Cortical_Pop.record("node[n](0.5).ref_e_extracellular", sampling_interval=rec_sampling_interval)
 
 
     # Assign Positions for recording and stimulating electrode point sources
