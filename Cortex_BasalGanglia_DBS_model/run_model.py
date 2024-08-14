3d684c8bfe5ec46f8d0fba195d59a14b7ac2e8df
Laura-Buckley <laura.buckley1@ucdconnect.ie>
2024-08-14 09:24:18 +0100

    -2 mA stim in the generate_signal

--- Cortex_BasalGanglia_DBS_model/run_model.py
+++ Cortex_BasalGanglia_DBS_model/run_model.py
@@ -562,7 +562,7 @@ if __name__ == "__main__":
             stop_time=sim_total_time,
             last_pulse_time_prior=last_pulse_time_prior,
             dt=simulator.state.dt,
-            amplitude=-1.0, #apply relevant values for cortical stimulation
+            amplitude=-2.0, #apply relevant values for cortical stimulation
             frequency=130.0,
             pulse_width=0.06,
             offset=0,
