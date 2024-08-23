b585fde9bdf2e6feced44cf9ed30042931c61724
Laura-Buckley <laura.buckley1@ucdconnect.ie>
2024-08-23 14:50:18 +0100

    0.05mA 70 hz

--- Cortex_BasalGanglia_DBS_model/run_model.py
+++ Cortex_BasalGanglia_DBS_model/run_model.py
@@ -584,7 +584,7 @@ if __name__ == "__main__":
             last_pulse_time_prior=last_pulse_time_prior,
             dt=simulator.state.dt,
             amplitude=-0.05, #apply relevant values for cortical stimulation
-            frequency=120.0,
+            frequency=70.0,
             pulse_width=0.06,
             offset=0,
         )
