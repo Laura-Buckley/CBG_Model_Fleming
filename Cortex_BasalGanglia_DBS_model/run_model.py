02a80c4d349952d602d0aaf0e130ad7a1a675eda
Laura-Buckley <laura.buckley1@ucdconnect.ie>
2024-08-28 12:28:37 +0100

    new electrode 80 hz 10uA

--- Cortex_BasalGanglia_DBS_model/run_model.py
+++ Cortex_BasalGanglia_DBS_model/run_model.py
@@ -584,7 +584,7 @@ if __name__ == "__main__":
             last_pulse_time_prior=last_pulse_time_prior,
             dt=simulator.state.dt,
             amplitude=-0.01, #apply relevant values for cortical stimulation
-            frequency=60.0,
+            frequency=80.0,
             pulse_width=0.06,
             offset=0,
         )
