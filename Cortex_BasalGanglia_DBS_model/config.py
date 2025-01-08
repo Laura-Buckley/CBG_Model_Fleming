e16a217182aac1350a153dcf2c55de923bd7fce4
Laura-Buckley <Laura.buckley1@ucdconnect.ie>
2025-01-08 15:03:28 +0000

    remove to lower case param in config

--- Cortex_BasalGanglia_DBS_model/config.py
+++ Cortex_BasalGanglia_DBS_model/config.py
@@ -110,7 +110,7 @@ class Config(object):
         stage_two_mean={"type": "boolean", "coerce": bool, "default": False},
         debug={"type": "boolean", "coerce": bool, "default": False},
         normalise_error={"type": "boolean", "coerce": bool, "default": True},
-        filename = {"type": "string", "coerce": (str, lambda x: x.lower()), "default": "boston"} #set up filename for coupled model - boston scientific
+        filename = {"type": "string", "coerce": str, "default": "boston"} #set up filename for coupled model - boston scientific
     )
 
     def __init__(self, config_file):
